7d5e2d4075fd051af60dd8ceb5f009f8d0c35757
Lucas de souza lameira <lucasslameira@gmail.com>
2020-06-25 05:28:38 -0300

    binary-lamp

--- binary/script.js
+++ binary/script.js
@@ -1,23 +1,62 @@
 var lamps = document.querySelectorAll('div.lamp');
-console.log(lamps);
+let fieldsetElement = document.getElementById('binary');
+let binaryElement = document.getElementById('binary');
+let decimalElement = document.getElementById('decimal')
+let total = 0;
 
-var turn = false;
+let arr = [0,0,0,0,0,0,0,0];
 
-function change(lamp) {    
-    if(!turn){
-        lamp.style.backgroundColor = 'yellow';           
-        turn = true        
+function change(lamp) {
+    if(lamp.style.backgroundColor === 'white'){    
+        lamp.style.backgroundColor = 'yellow';
+        binario(lamp.id);
+        sum(lamp.id);                                     
+        return;
     }else{
-        lamp.style.backgroundColor = 'white';           
-        turn = false        
-    }   
-    console.log(turn)
+        lamp.style.backgroundColor = 'white';
+        tiraBinario(lamp.id);        
+        subtract(lamp.id)
+        return;
+    }        
+}
+
+function binario (id) {
+    //console.log(id);//string             
+    let k = 1;      
+    binaryElement.innerHTML = '';    
+    for(let i = 0; i<7; i++){        
+        if(id==k)
+            arr[i] = 1;            
+        k+=k;
+       binaryElement.appendChild(document.createTextNode(arr[i]));        
+    }          
+}
+
+function tiraBinario(id) {
+    let k = 1;
+    binaryElement.innerHTML = '';
+    for(let i = 0; i<arr.length; i++){
+        if(id==k)
+            arr[i] = 0;
+        k+=k
+        binaryElement.appendChild(document.createTextNode(arr[i]));        
+    }
+}
+
+function sum(id){
+    decimalElement.innerHTML = '';
+    total += Number(id);    
+    decimalElement.appendChild(document.createTextNode(total))
+    
+}
+
+function subtract (id) {
+    decimalElement.innerHTML = '';
+    total -= Number(id)
+    decimalElement.appendChild(document.createTextNode(total))
 }
 
 lamps.forEach(lamp => {
-    console.log(lamp);
-    lamp.addEventListener('click', () => change(lamp));    
+    lamp.style.backgroundColor = 'white';
+    lamp.addEventListener('click', () => change(lamp));
 })
-
-
-
